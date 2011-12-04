7c4b2b677eed66affb85e9d8dad6b0705aee7d45
Jens Stalder <metalray@me.com>
2011-12-04 17:32:54 +0100

    sample config

---
+++ mr_config_example.php
@@ -0,0 +1,29 @@
+<?php
+
+/* DB */
+$DB_HOST	= "localhost";
+$DB_BN		= "db131999_andenma";
+$DB_PW		= "giraffe33";
+$DB_DB		= "db131999_andenmatten";
+
+/* GLOBALS */
+$tableprefix = 'andenmatten';
+$root = 'http://devpress.local';
+$theme = 'franz';
+$themeindex = 'kunden.php';
+
+$themeurl = $root . '/themes/' . $theme;
+$themeindexurl = $themeurl . '/' . $themeindex;
+
+//MySQL connect
+$con = mysql_connect($DB_HOST, $DB_BN, $DB_PW);
+if (!$con) {
+    die('Could not connect: ' . mysql_error());
+}
+mysql_select_db($DB_DB);
+mysql_query("SET NAMES utf8");
+mysql_query("SET collation_connection = 'utf8_unicode_ci'");
+
+
+
+?>
